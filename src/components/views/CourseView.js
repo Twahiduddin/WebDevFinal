3f8f27c56232e4b19132d6056b34b173588b45bf
Twahid Uddin <Twahiduddin@aol.com>
2022-05-23 17:25:00 -0400

    Course View edited to add timeslot and location and other dependancies

--- src/components/views/CourseView.js
+++ src/components/views/CourseView.js
@@ -1,12 +1,42 @@
+import { useHistory } from "react-router";
+
 const CourseView = (props) => {
   const { course } = props;
+  const history = useHistory();
+
+  const gotoinstructor = (id) => {
+    history.push(`/instructor/${id}`);
+  };
+
+  const editCourse = (id) => {
+    history.push(`/editcourse/${id}`);
+  };
+
   return (
     <div>
-      <h1>{course.title}</h1>
-      {course.instructor ? <h3>{course.instructor.firstname + " " + course.instructor.lastname}</h3>: <h3>staff</h3>}
+      <div>
+        <h1>{course.title}</h1>
+        <h3>{course.timeslot}</h3>
+        <h3>{course.location}</h3>
+        <button onClick={() => editCourse(course.id)}>Edit</button>
+      </div>
+
+      {course.instructor ? (
+        <>
+          <h3
+            style={{ cursor: "pointer" }}
+            onClick={() => gotoinstructor(course.instructor.id)}
+          >
+            {course.instructor.firstname + " " + course.instructor.lastname}
+          </h3>
+        </>
+      ) : (
+        <>
+          <h3>Staff</h3>
+        </>
+      )}
     </div>
   );
-
 };
 
 export default CourseView;

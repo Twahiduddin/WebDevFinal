f97bddb5d41af3ac095d56711b16b68e0a5c28c3
Twahid Uddin <Twahiduddin@aol.com>
2022-05-23 17:09:26 -0400

    Added all the routes and dependencies for app.js, even though for some of the routes the containers and views aren't created yet. This is because once I do create them it'll be easier to just connect to the routes.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 
 //Router
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, Link } from "react-router-dom";
 //Components
 import {
   HomePageContainer,
@@ -9,16 +9,23 @@ import {
   CourseContainer,
   AllInstructorsContainer,
   AllCoursesContainer,
-  NewCourseContainer
-} from './components/containers';
+  NewCourseContainer,
+} from "./components/containers";
+import NewInstructorComponent from "./components/containers/NewInstructorComponent";
+import EditCourseContainer from "./components/containers/EditCourseContainer";
+import EditInstructorContainer from "./components/containers/EditInstructorContainer";
 
-// if you create separate components for adding/editing 
+// if you create separate components for adding/editing
 // a student or instructor, make sure you add routes to those
 // components here
 
 const App = () => {
   return (
     <div className="App">
+      <div>
+        <Link to={"/instructors"}> All Instructors </Link>&nbsp;&nbsp;
+        <Link to={"/courses"}> All Courses </Link>
+      </div>
       <Switch>
         <Route exact path="/" component={HomePageContainer} />
         <Route exact path="/instructors" component={AllInstructorsContainer} />
@@ -26,11 +33,16 @@ const App = () => {
         <Route exact path="/courses" component={AllCoursesContainer} />
         <Route exact path="/newcourse" component={NewCourseContainer} />
         <Route exact path="/course/:id" component={CourseContainer} />
-
-      </Switch>        
+        <Route exact path="/newinstructor" component={NewInstructorComponent} />
+        <Route exact path="/editcourse/:id" component={EditCourseContainer} />
+        <Route
+          exact
+          path="/editinstructor/:id"
+          component={EditInstructorContainer}
+        />
+      </Switch>
     </div>
   );
-}
+};
 
 export default App;
-
